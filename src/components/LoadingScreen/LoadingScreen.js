de5cba1feed726d7e6008feb9aa449c88c0b68fd
Adebola <adebola.aladesuru@radicalcompany.com>
2019-05-29 16:23:32 +0100

    add new component

---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-screen-container">
+      <span>Loading post</span>
+      <span />
+    </div>
+  );
+};
+
+export default LoadingScreen;
