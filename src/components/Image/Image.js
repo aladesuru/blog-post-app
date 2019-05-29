d40c84b3ca183b6b6961eda89e45ef1c078312fa
Adebola <adebola.aladesuru@radicalcompany.com>
2019-05-29 19:08:00 +0100

    add logic if image does not exist

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import alternativeImage from "../../assets/images/slider-image-1.jpg";
+
+const handleOnError = e => {
+  e.target.src = alternativeImage;
+};
 
 const Image = ({ url }) => {
-  return <img src={url} alt="post" />;
+  return <img src={url} alt="post" onError={handleOnError} />;
 };
 
 Image.prototype = {
